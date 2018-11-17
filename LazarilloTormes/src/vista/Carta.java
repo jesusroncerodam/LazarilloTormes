eb0e76e0f3ceb5d6c3bd878b520405e611588820
gmg99 <gmg99@github.com>
2018-11-17 12:51:46 +0100

    t

--- LazarilloTormes/src/vista/Carta.java
+++ LazarilloTormes/src/vista/Carta.java
@@ -1,13 +1,10 @@
-/*
- To change this license header, choose License Headers in Project Properties.
- To change this template file, choose Tools | Templates
- and open the template in the editor.
+/**
+ * JLabel que se muesta , cada carta, gestina cuando se muee 
  */
 package vista;
 
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -25,42 +22,53 @@ import javax.swing.border.Border;
  */
 public class Carta extends JLabel {
 
-    private ImageIcon aux, vuelta;
+    private ImageIcon imgCarta, imgVuelta;
     private String url;
-    private final Border borde = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.blue, 2), BorderFactory.createLoweredBevelBorder());
-    final int WIDTH_DEF, HEIHT_DEF;
- //   private final int ANCHOMAX = 100, ALTOMAX = 100;
-    int altura, ancho;
+    private final Border borde = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.black, 1), BorderFactory.createLoweredBevelBorder());
+    private int altura, ancho;
     private boolean sale;
     private Timer timer;
     private boolean bloquear;
+    
+    private final String VUELTA_IMAGEN="src/img/cartas/imgVuelta.png";
+    private final int WIDTH_DEF, HEIHT_DEF,TIEMPO_ANIMACION=2;
 
 
-
+    /**
+     * Constructor de la Carta se le manda la localizacion de la carta
+     * @param url 
+     */
     public Carta(String url) {
-        bloquear=false;
-        this.setBorder(borde);
+        this.setBorder(borde);//ponemos un borde
+        imgVuelta = new ImageIcon("src/img/cartas/vuelta.png");//("src/img/cartas/vuelta.png");////////////////////////////////////////////////////////ERROR SI SE LE PONE LA LOICALIZACION POR VARIABLE NO VA
+        this.setSize(imgVuelta.getIconWidth(),imgVuelta.getIconHeight());//la imagen de "imgVuelta" da las dimensiones
+        this.setIcon(imgVuelta);//asignamos como icono la imagen de imgVuelta
+        imgCarta = cambiarTamano(new ImageIcon(url), imgVuelta.getIconWidth(), imgVuelta.getIconHeight());//asignamos un tamaño a la carta, para que esta se vea con las mismas 
+        
+        //asignamios datos
         this.url = url;
+        bloquear=false;
         sale=false;
-        vuelta = new ImageIcon("src/img/cartas/vuelta.png");//cambiarTamano(new ImageIcon("src/img/cartas/vuelta.png"),100,300);
-        this.setSize(vuelta.getIconWidth(),vuelta.getIconHeight());
-        WIDTH_DEF=vuelta.getIconWidth();
-        HEIHT_DEF=vuelta.getIconHeight();
-      //  this.setMinimumSize(new Dimension(,));
-        //carta=cambiarTamano(new ImageIcon("src/img/cartas/vuelta.png"),100,100);
-        this.setIcon(vuelta);
-        // aux=cambiarTamano(new ImageIcon("src/img/logotrini.png"),100,100);
-        aux = cambiarTamano(new ImageIcon(url), vuelta.getIconWidth(), vuelta.getIconHeight());
-        // this.setOpaque(false);
+        WIDTH_DEF=imgVuelta.getIconWidth();//debe de estar despues de asignarle la imagen a imgVuelta
+        HEIHT_DEF=imgVuelta.getIconHeight();//debe de estar despues de asignarle la imagen a imgVuelt
     }
 
-
+    /**
+     * Pintamos el fondo , el fondo siempre estará pero dentra una imagen encima 
+     * o no , dependiendo del estado en el que se encuentre
+     * @param g 
+     */
     public void paint(Graphics g) {
         //imagen de fondo, la carta
-        g.drawImage(aux.getImage(), 0, 0, null);
+        g.drawImage(imgCarta.getImage(), 0, 0, null);
         super.paint(g);
     }
     
+    /**
+     * Animamos la imagen, ya sea la anumacion de entrada o de salida, esto lo 
+     * controla la propia carta, anima simpre que se pueda, si la carta esta 
+     * bloqueada no va a animar
+     */
     public void animar(){
         if(!bloquear){
             if(!sale){
@@ -70,36 +78,51 @@ public class Carta extends JLabel {
             }
         }
     }
-
-    public void animarSalir() {
-        //System.out.println("icono=" + vuelta.getIconHeight() + " " + vuelta.getIconWidth());
-       // System.out.println("contenedor=" + this.getHeight() + " " + this.getWidth());
-        // this.setMaximumSize(new Dimension(this.getWidth(), getHeight()));
-        altura = vuelta.getIconHeight();
-        ancho = vuelta.getIconWidth();
+    
+    /**
+     *  Comienza la animacion de salida, la imagen desaparece poco a poco
+     *  coge las dimensiones de la uimagen de vuelta y las almacena para ir
+     *  reduciendo poco a poco su tamaño
+     */
+    private void animarSalir() {
+        altura = imgVuelta.getIconHeight();
+        ancho = imgVuelta.getIconWidth();
         sale = true;
         empezar();
     }
 
-
-    public void animarEntrar() {
-        this.setIcon(cambiarTamano((ImageIcon) vuelta, 2, 2));
+    /**
+     * Comienza la animacion de entrar, la imagen va apareciendo poco a poco
+     * se asigna un icono para que lo aumente con un tamaño minimo puesto que 
+     * si esta en animarEntrar antes a hecho la animoacion de salir y la imagen 
+     * sería null
+     */
+    private void animarEntrar() {
+        this.setIcon(cambiarTamano((ImageIcon) imgVuelta, 2, 2));
         sale = false;
         empezar();
     }
 
-
-    public void empezar() {
-        if (timer != null) {
+    /**
+     * Casigna el timer , si hay una animacion en progreso, la pausa y crea una 
+     * Esta hecho para que no entre en bucle cuando haces click en la isma imagen
+     * mas de una vez mientras estaba apareciendo; Cuando asigna el timer dependiendo
+     * de el estado de sale, si esta en animacion entrar o anumacion salir, aumentara o
+     * disminuira el tamano de la imagen
+     */
+    private void empezar(){
+        if (timer != null){//si existe un timer lo paramos, evitamos tener 2 timer,
+                            //uno aumentando el tamano y otro restando el tamano
             timer.stop();
         }
-        timer = new Timer(2, new ActionListener() {
+        
+        timer = new Timer(TIEMPO_ANIMACION, new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e) {
-                if (sale) {
+            public void actionPerformed(ActionEvent e){
+                if(sale){
                     altura--;
                     ancho--;
-                } else {
+                }else{
                     altura++;
                     ancho++;
                 }
@@ -112,16 +135,14 @@ public class Carta extends JLabel {
 
 
     
-    public void ponerImagen() {
-        //System.out.println(altura + " " + ancho);
-        if (altura > 0 && ancho > 0 && altura < vuelta.getIconHeight() && ancho < vuelta.getIconWidth()) {
-        //    System.out.println("poniendo img");
-            this.setIcon(cambiarTamano(new ImageIcon("src/img/cartas/vuelta.png"), ancho, altura));
-        } else {
+    private void ponerImagen() {
+        if(altura>0 && ancho>0 && altura<imgVuelta.getIconHeight() && ancho<imgVuelta.getIconWidth()){
+            this.setIcon(cambiarTamano(new ImageIcon("src/img/cartas/vuelta.png"), ancho, altura));//"src/img/cartas/vuelta.png"
+        }else{
             if (sale) {
                 setIcon(null);
             } else {
-                setIcon(vuelta);
+                setIcon(imgVuelta);
             }
             timer.stop();
         }
@@ -138,7 +159,7 @@ public class Carta extends JLabel {
     public void bloquear() {
         bloquear=true;
         repaint();
-        this.setIcon(aux);//cambiarTamano(aux,WIDTH_DEF,HEIHT_DEF));
+        this.setIcon(imgCarta);//cambiarTamano(imgCarta,WIDTH_DEF,HEIHT_DEF));
     }
 
     public String getUrl() {
