ea632ef2a990d7959144d247860785e9b4b47db2
jesusroncerodam <44838341+jesusroncerodam@users.noreply.github.com>
2018-11-20 08:44:04 +0100

    846

--- LazarilloTormes/src/trabajodi/Logica.java
+++ LazarilloTormes/src/trabajodi/Logica.java
@@ -5,6 +5,7 @@
  */
 package trabajodi;
 
+
 import controladores.ContrJuego;
 import java.awt.Component;
 import java.io.BufferedReader;
@@ -31,29 +32,36 @@ import javax.swing.JLabel;
 
  @author Guille
  */
-public class Logica implements Serializable{
+public class Logica implements Serializable {
+
     private ContrJuego juego;
-    private boolean primeraJuego,animacionC;
+    private boolean primeraJuego, animacionC;
     private Timer timer;
-    private  int vuelta,cartaAct;
-    private final String FICHERO="estadisticas.txt",PRIMERA_LINEA="Directorio de almacenamiento de estadistica\n";
-    
+    private int vuelta, cartaAct;
+    private final String FICHERO = "estadisticas.txt", PRIMERA_LINEA = "Directorio de almacenamiento de estadistica\n";
+
+
     public Logica() {
     }
-    
+
+
     /**
-     * asinamos el controlador de Juego y incicializamos valores a un juego por defecto
-     * @param juego ContrJuego encargado de la comunicacion con el juego
+     asinamos el controlador de Juego y incicializamos valores a un juego por
+     defecto
+     @param juego ContrJuego encargado de la comunicacion con el juego
      */
-    public void asignarContrJuego(ContrJuego juego){
-        this.juego=juego;
-        primeraJuego=true;
-        animacionC=false;
+    public void asignarContrJuego(ContrJuego juego) {
+        this.juego = juego;
+        primeraJuego = true;
+        animacionC = false;
     }
-    
-    public void Menus(String accion){
-        
+
+
+    public void Menus(String accion) {
+
     }
+
+
     /*
      // CONTROLADOR VISTA JUEGO
      // CONTROLADOR VISTA JUEGO
@@ -65,76 +73,77 @@ public class Logica implements Serializable{
      // CONTROLADOR VISTA JUEGO
      // CONTROLADOR VISTA JUEGO
      */
-           
+
     /**
-     * Encargado de gestionar las acciones de de mouseListener del controlador 
-     * del juego, aqui se gestionaran toda la logica
-     * @param componente 
+     Encargado de gestionar las acciones de de mouseListener del controlador
+     del juego, aqui se gestionaran toda la logica
+     @param componente
      */
     public synchronized void juegoClick(Component componente) {
         //si se ejecuta laguna accion  comienza el contador
-        if(primeraJuego){
+        if (primeraJuego) {
             juego.gestionarContador("empezar");
-            primeraJuego=false;//ya no es la 1 accion del juego
+            primeraJuego = false;//ya no es la 1 accion del juego
         }
-        
-        if(componente instanceof JButton){//si es un boton
-            String accion=((JButton) componente).getActionCommand();//guardamos la accion del boton
+
+        if (componente instanceof JButton) {//si es un boton
+            String accion = ((JButton) componente).getActionCommand();//guardamos la accion del boton
             switch (accion) {
                 case "playPause":
                     juego.gestionarContador(accion);
-                    break; 
-                    
+                    break;
+
                 case "continuar"://guardar los datos
                     guardarDatos();
                     break;
-                    
+
                 case "guardar":
                     System.out.println("en creaccion");
                     break;
-                    
+
                 default:
-                    System.out.println("error juegoClick opcion no esperada: "+accion);
+                    System.out.println("error juegoClick opcion no esperada: " + accion);
             }
-            
-        }else if(componente instanceof JLabel){//si es un JLabel, 
-            if(!animacionC){//mientras que no tengamos ninguna animacion en progreso
-                JLabel jlComponente=(JLabel) componente;
-                
-                vuelta=juego.algunaVisible();//antes de mover la carta actual miramos si hay alguna carta visible
-                cartaAct=Integer.parseInt(jlComponente.getName());//opbtenemos el indice del array de la carta que ss hizo click
-                
-                if(vuelta==cartaAct){//comprobamos que no tenga hecho click en la misma imagen 2 veces
-                    vuelta=-1;//si es asi la carta que se dio la vuelta no existe, para la logica
+
+        } else if (componente instanceof JLabel) {//si es un JLabel, 
+            if (!animacionC) {//mientras que no tengamos ninguna animacion en progreso
+                JLabel jlComponente = (JLabel) componente;
+
+                vuelta = juego.algunaVisible();//antes de mover la carta actual miramos si hay alguna carta visible
+                cartaAct = Integer.parseInt(jlComponente.getName());//opbtenemos el indice del array de la carta que ss hizo click
+
+                if (vuelta == cartaAct) {//comprobamos que no tenga hecho click en la misma imagen 2 veces
+                    vuelta = -1;//si es asi la carta que se dio la vuelta no existe, para la logica
                 }
                 juego.movimiento();//se realiza 1 movimiento, sumamos 1 al contador
 
                 juego.girar(cartaAct);//gira mos la carta que se hizo click
-                
-                if(vuelta!=-1){//si es diferente de  -1 hay 2 visibles
-                    if(juego.mismaImagen(vuelta, cartaAct)){//si las cartas que hay son iguales
-                        
+
+                if (vuelta != -1) {//si es diferente de  -1 hay 2 visibles
+                    if (juego.mismaImagen(vuelta, cartaAct)) {//si las cartas que hay son iguales
+
                         juego.bloquearImagenes(vuelta, cartaAct);//bloqueamos la imagen, para que no se puedan mover mas
 
-                        if(!juego.isFin()){//si es el fin del juego(Si a termiando)
+                        if (!juego.isFin()) {//si es el fin del juego(Si a termiando)
                             juego.gestionarContador("pausa");
                             //cambiamos los estados de los botones 
                             juego.cambiarEstadoBoton("guardar", false);
                             juego.cambiarEstadoBoton("playPause", false);
                             juego.cambiarEstadoBoton("continuar", true);
                         }
-                        
-                    }else{//si las cartas que tenemos no son iguales las giramos
-                        animacionC=true;//ponermos que existe una animacion en progreso
-                        timer=new java.util.Timer();//cremos el timer para crear una animacion
-                        TimerTask tarea =new TimerTask() {//creamos un timerTask, que se ejecutara en x segundos
+
+                    } else {//si las cartas que tenemos no son iguales las giramos
+                        animacionC = true;//ponermos que existe una animacion en progreso
+                        timer = new java.util.Timer();//cremos el timer para crear una animacion
+                        TimerTask tarea = new TimerTask() {//creamos un timerTask, que se ejecutara en x segundos
                             @Override
                             public void run() {
                                 juego.girar(cartaAct);//girampos la carta actual 
                                 juego.girar(vuelta);//girampos la que esta visble
-                                timer=null;//eliminamos el timer
-                                animacionC=false;//marcamos  que el timer termino
-                        }};
+                                timer = null;//eliminamos el timer
+                                animacionC = false;//marcamos  que el timer termino
+                            }
+                        };
                         //asignamos que se mueva en estos seguntos para que no cambie de golpe , para que la animacion sea mas suave
                         timer.schedule(tarea, 1000);// decimos al timer que ejecute el TimeTask en los seguntos
                     }
@@ -142,27 +151,30 @@ public class Logica implements Serializable{
             }
         }
     }
-    
-    private void guardarDatos(){
+
+
+    private void guardarDatos() {
         //nombre//ya lo tengo 
         //imagen //ya lo tengo
         //movimientos
         juego.getContMov();
         //tiempo
         juego.getContadorSeg();
-        
+
     }
-    
+
+
     public void juegokey() {
-        
+
     }
-    
-    private void gestionFichero(Historial historialNuevo){
+
+
+    private void gestionFichero(Historial historialNuevo) {
         ArrayList<Historial> historial;
         //nos aseguramos que existe un fichero
         crearFichero(true);
         //añadimos el contenido del fichero a un array
-        historial= pasarFicheroAArray();
+        historial = pasarFicheroAArray();
         //añadimos los nuevos valores 
         historial.add(historialNuevo);
         //ordenamos el arraylist
@@ -170,74 +182,79 @@ public class Logica implements Serializable{
         //lo escribimos en el fichero
         pasarAFichero(historial);
     }
+
     //        fichero.delete();
 
-    private void crearFichero(boolean mantenerFichero){
-        try{
+    private void crearFichero(boolean mantenerFichero) {
+        try {
             File archivo = new File(FICHERO);
             System.out.println(archivo.getAbsoluteFile());
-            if(!archivo.exists()){
-                FileWriter escritor=new FileWriter(archivo,mantenerFichero);//true no sobrescribe
+            if (!archivo.exists()) {
+                FileWriter escritor = new FileWriter(archivo, mantenerFichero);//true no sobrescribe
                 escritor.write(PRIMERA_LINEA);
                 escritor.close();
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error al escribir");//Si existe un problema al escribir cae aqui
         }
     }
-    
+
+
     public void mostrarFichero() {
-    //Creamos un String que va a contener todo el texto del archivo
-            String texto = "";
-            System.out.println("ficheroooooo");
-            System.out.println("================================");
-            try {
-    //Creamos un archivo FileReader que obtiene lo que tenga el archivo
-                FileReader lector = new FileReader(FICHERO);
+        //Creamos un String que va a contener todo el texto del archivo
+        String texto = "";
+        System.out.println("ficheroooooo");
+        System.out.println("================================");
+        try {
+            //Creamos un archivo FileReader que obtiene lo que tenga el archivo
+            FileReader lector = new FileReader(FICHERO);
 
-    //El contenido de lector se guarda en un BufferedReader
-                BufferedReader contenido = new BufferedReader(lector);
+            //El contenido de lector se guarda en un BufferedReader
+            BufferedReader contenido = new BufferedReader(lector);
 
-    //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
-                while ((texto = contenido.readLine()) != null) {
-                    System.out.println(texto);
-                }
-            } //Si se causa un error al leer cae aqui
-            catch (Exception e) {
-                System.out.println("Error al leer");
+            //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+            while ((texto = contenido.readLine()) != null) {
+                System.out.println(texto);
             }
+        } //Si se causa un error al leer cae aqui
+        catch (Exception e) {
+            System.out.println("Error al leer");
         }
-    
-    public void test(){
-        ArrayList historial=new ArrayList<Historial>();
+    }
+
+
+    public void test() {
+        ArrayList historial = new ArrayList<Historial>();
         //String nombre, ImageIcon imagen, int tiempo, int movimientos
-        historial.add(new Historial( 20, 10,"url","em"));
-        historial.add(new Historial( 50, 15,"url","am"));
-        historial.add(new Historial( 20, 15,"url","om"));
-        historial.add(new Historial( 2, 10,"url","ma"));
-        historial.add(new Historial(50, 18,"url","me"));
+        historial.add(new Historial(20, 10, "url", "em"));
+        historial.add(new Historial(50, 15, "url", "am"));
+        historial.add(new Historial(20, 15, "url", "om"));
+        historial.add(new Historial(2, 10, "url", "ma"));
+        historial.add(new Historial(50, 18, "url", "me"));
         pasarAFichero(historial);
         /*
-        for (Object object : historial) {
-            System.out.println(object.toString()+"");
-        }
-        Collections.sort(historial);
-        System.out.println("----");
-        for (Object object : historial) {
-            System.out.println(object.toString()+"");
-        }*/
+         for (Object object : historial) {
+         System.out.println(object.toString()+"");
+         }
+         Collections.sort(historial);
+         System.out.println("----");
+         for (Object object : historial) {
+         System.out.println(object.toString()+"");
+         }
+         */
     }
-    
-    public ArrayList<Historial> pasarFicheroAArray(){//Retorna el contenido del fichero en el array
+
+
+    public ArrayList<Historial> pasarFicheroAArray() {//Retorna el contenido del fichero en el array
         String linea;
         String[] lineas;
-        ArrayList<Historial> historial=new ArrayList();
+        ArrayList<Historial> historial = new ArrayList();
         try {
             FileReader f = new FileReader(FICHERO);
             BufferedReader b = new BufferedReader(f);
             b.readLine();//ignoramos la 1º linea
-            while((linea = b.readLine())!=null) {
-                lineas=linea.split(";");//movimientos;tiempo;imagen;nombre
+            while ((linea = b.readLine()) != null) {
+                lineas = linea.split(";");//movimientos;tiempo;imagen;nombre
                 historial.add(new Historial(Integer.parseInt(lineas[0]), Integer.parseInt(lineas[1]), lineas[2], lineas[3]));
             }
             b.close();
@@ -248,10 +265,11 @@ public class Logica implements Serializable{
         }
         return historial;
     }
-    
-    public void pasarAFichero(ArrayList<Historial> historial){//coge el arrayList y lo escribe en el fichero
+
+
+    public void pasarAFichero(ArrayList<Historial> historial) {//coge el arrayList y lo escribe en el fichero
         try {
-            FileWriter fichero = new FileWriter(FICHERO,false);
+            FileWriter fichero = new FileWriter(FICHERO, false);
             fichero.write(PRIMERA_LINEA);
             for (Historial historiales : historial) {
                 fichero.write(historiales.toString() + "\n");
@@ -261,19 +279,19 @@ public class Logica implements Serializable{
         } catch (Exception ex) {
             System.out.println("Mensaje de la excepción: " + ex.getMessage());
         }
-        
+
     }
 
-            
-    public String[] ficheroAArray(){
+
+    public String[] ficheroAArray() {
         String linea;
         ArrayList<String> lineas = new ArrayList();
         try {
             FileReader f = new FileReader(FICHERO);
             BufferedReader b = new BufferedReader(f);
             b.readLine();//ignoramos la 1º linea
-            while((linea = b.readLine())!=null) {
-                lineas.add(""+linea);//movimientos;tiempo;imagen;nombre
+            while ((linea = b.readLine()) != null) {
+                lineas.add("" + linea);//movimientos;tiempo;imagen;nombre
             }
             b.close();
         } catch (FileNotFoundException ex) {
@@ -287,6 +305,8 @@ public class Logica implements Serializable{
         System.out.println("==========================");
         return (String[]) lineas.toArray(new String[lineas.size()]);
     }
+
+
     /*
      // CONTROLADOR VISTA CARGA
      // CONTROLADOR VISTA CARGA
@@ -333,9 +353,9 @@ public class Logica implements Serializable{
     }
 
 
-    public synchronized void dormir(int tiempo){
+    public synchronized void dormir(int tiempo) {
         try {
-            Thread.sleep(tiempo*1000);
+            Thread.sleep(tiempo * 1000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Logica.class.getName()).log(Level.SEVERE, null, ex);
         }
--- LazarilloTormes/src/vista/Carta.java
+++ LazarilloTormes/src/vista/Carta.java
@@ -1,5 +1,5 @@
 /**
- * JLabel que se muesta , cada carta, gestina cuando se muee 
+ JLabel que se muesta , cada carta, gestina cuando se muee
  */
 package vista;
 
@@ -21,7 +21,7 @@ import javax.swing.border.Border;
 
  @author Guille
  */
-public class Carta extends JLabel implements Serializable{
+public class Carta extends JLabel implements Serializable {
 
     private ImageIcon imgCarta, imgVuelta;
     private String url;
@@ -30,60 +30,63 @@ public class Carta extends JLabel implements Serializable{
     private boolean sale;
     private Timer timer;
     private boolean bloquear;
-    
-    private final String VUELTA_IMAGEN="src/img/cartas/vuelta.png";
-    private final int WIDTH_DEF, HEIHT_DEF,TIEMPO_ANIMACION=2;
+
+    private final String VUELTA_IMAGEN = "src/img/cartas/vuelta.png";
+    private final int WIDTH_DEF, HEIHT_DEF, TIEMPO_ANIMACION = 2;
 
 
     /**
-     * Constructor de la Carta se le manda la localizacion de la carta
-     * @param url 
+     Constructor de la Carta se le manda la localizacion de la carta
+     @param url
      */
     public Carta(String url) {
         this.setBorder(borde);//ponemos un borde
         imgVuelta = new ImageIcon(VUELTA_IMAGEN);
-        this.setSize(imgVuelta.getIconWidth(),imgVuelta.getIconHeight());//la imagen de "imgVuelta" da las dimensiones
+        this.setSize(imgVuelta.getIconWidth(), imgVuelta.getIconHeight());//la imagen de "imgVuelta" da las dimensiones
         this.setIcon(imgVuelta);//asignamos como icono la imagen de imgVuelta
         imgCarta = cambiarTamano(new ImageIcon(url), imgVuelta.getIconWidth(), imgVuelta.getIconHeight());//asignamos un tamaño a la carta, para que esta se vea con las mismas 
-        
+
         //asignamios datos
         this.url = url;
-        bloquear=false;
-        sale=false;
-        WIDTH_DEF=imgVuelta.getIconWidth();//debe de estar despues de asignarle la imagen a imgVuelta
-        HEIHT_DEF=imgVuelta.getIconHeight();//debe de estar despues de asignarle la imagen a imgVuelt
+        bloquear = false;
+        sale = false;
+        WIDTH_DEF = imgVuelta.getIconWidth();//debe de estar despues de asignarle la imagen a imgVuelta
+        HEIHT_DEF = imgVuelta.getIconHeight();//debe de estar despues de asignarle la imagen a imgVuelt
     }
 
+
     /**
-     * Pintamos el fondo , el fondo siempre estará pero dentra una imagen encima 
-     * o no , dependiendo del estado en el que se encuentre
-     * @param g 
+     Pintamos el fondo , el fondo siempre estará pero dentra una imagen encima
+     o no , dependiendo del estado en el que se encuentre
+     @param g
      */
     public void paint(Graphics g) {
         //imagen de fondo, la carta
         g.drawImage(imgCarta.getImage(), 0, 0, null);
         super.paint(g);
     }
-    
+
+
     /**
-     * Animamos la imagen, ya sea la anumacion de entrada o de salida, esto lo 
-     * controla la propia carta, anima simpre que se pueda, si la carta esta 
-     * bloqueada no va a animar
+     Animamos la imagen, ya sea la anumacion de entrada o de salida, esto lo
+     controla la propia carta, anima simpre que se pueda, si la carta esta
+     bloqueada no va a animar
      */
-    public void animar(){
-        if(!bloquear){
-            if(!sale){
+    public void animar() {
+        if (!bloquear) {
+            if (!sale) {
                 animarSalir();
-            }else{
+            } else {
                 animarEntrar();
             }
         }
     }
-    
+
+
     /**
-     *  Comienza la animacion de salida, la imagen desaparece poco a poco
-     *  coge las dimensiones de la uimagen de vuelta y las almacena para ir
-     *  reduciendo poco a poco su tamaño
+     Comienza la animacion de salida, la imagen desaparece poco a poco
+     coge las dimensiones de la uimagen de vuelta y las almacena para ir
+     reduciendo poco a poco su tamaño
      */
     private void animarSalir() {
         altura = imgVuelta.getIconHeight();
@@ -92,11 +95,12 @@ public class Carta extends JLabel implements Serializable{
         empezar();
     }
 
+
     /**
-     * Comienza la animacion de entrar, la imagen va apareciendo poco a poco
-     * se asigna un icono para que lo aumente con un tamaño minimo puesto que 
-     * si esta en animarEntrar antes a hecho la animoacion de salir y la imagen 
-     * sería null
+     Comienza la animacion de entrar, la imagen va apareciendo poco a poco
+     se asigna un icono para que lo aumente con un tamaño minimo puesto que
+     si esta en animarEntrar antes a hecho la animoacion de salir y la imagen
+     sería null
      */
     private void animarEntrar() {
         this.setIcon(cambiarTamano((ImageIcon) imgVuelta, 2, 2));
@@ -104,26 +108,29 @@ public class Carta extends JLabel implements Serializable{
         empezar();
     }
 
+
     /**
-     * Casigna el timer , si hay una animacion en progreso, la pausa y crea una 
-     * Esta hecho para que no entre en bucle cuando haces click en la isma imagen
-     * mas de una vez mientras estaba apareciendo; Cuando asigna el timer dependiendo
-     * de el estado de sale, si esta en animacion entrar o anumacion salir, aumentara o
-     * disminuira el tamano de la imagen
+     Casigna el timer , si hay una animacion en progreso, la pausa y crea una
+     Esta hecho para que no entre en bucle cuando haces click en la isma imagen
+     mas de una vez mientras estaba apareciendo; Cuando asigna el timer
+     dependiendo
+     de el estado de sale, si esta en animacion entrar o anumacion salir,
+     aumentara o
+     disminuira el tamano de la imagen
      */
-    private void empezar(){
-        if (timer != null){//si existe un timer lo paramos, evitamos tener 2 timer,
-                            //uno aumentando el tamano y otro restando el tamano
+    private void empezar() {
+        if (timer != null) {//si existe un timer lo paramos, evitamos tener 2 timer,
+            //uno aumentando el tamano y otro restando el tamano
             timer.stop();
         }
-        
-        timer = new Timer(TIEMPO_ANIMACION, new ActionListener(){
+
+        timer = new Timer(TIEMPO_ANIMACION, new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-                if(sale){
+            public void actionPerformed(ActionEvent e) {
+                if (sale) {
                     altura--;
                     ancho--;
-                }else{
+                } else {
                     altura++;
                     ancho++;
                 }
@@ -136,22 +143,23 @@ public class Carta extends JLabel implements Serializable{
 
 
     /**
-     * Lo llama el timer, coloca la imagen  Mientras que la imagen este entre 
-     * las orquillas del tamaño siendo este el minimo 0 y el maximo el tamaño
-     * de la imagen de imgVuelta
-     * En el caso de que se saliese de estas orquillas, si la imagen estaba saliendo
-     * la quita y si la imgen estaba entrando la asigna por defecto; siempre
-     * que se sale de las oquillas para el timer, puesto que termino la animacion
-     * 
-     * Siempte actualiza la carta para que los cambios sean visibles
+     Lo llama el timer, coloca la imagen Mientras que la imagen este entre
+     las orquillas del tamaño siendo este el minimo 0 y el maximo el tamaño
+     de la imagen de imgVuelta
+     En el caso de que se saliese de estas orquillas, si la imagen estaba
+     saliendo la quita y si la imgen estaba entrando la asigna por defecto;
+     siempre que se sale de las oquillas para el timer, puesto que termino la
+     animacion
+
+     Siempte actualiza la carta para que los cambios sean visibles
      */
     private void ponerImagen() {
-        if(altura>0 && ancho>0 && altura<imgVuelta.getIconHeight() && ancho<imgVuelta.getIconWidth()){
+        if (altura > 0 && ancho > 0 && altura < imgVuelta.getIconHeight() && ancho < imgVuelta.getIconWidth()) {
             this.setIcon(cambiarTamano(new ImageIcon(VUELTA_IMAGEN), ancho, altura));
-        }else{
-            if(sale){
+        } else {
+            if (sale) {
                 setIcon(null);
-            }else{
+            } else {
                 setIcon(imgVuelta);
             }
             timer.stop();
@@ -159,39 +167,42 @@ public class Carta extends JLabel implements Serializable{
         this.updateUI();
     }
 
+
     /**
-     * Manda si esta carta esta activada, si se ve la imgen de la carta
-     * Si esta bloqueada retornara false puesto que esta carta ya no tiene
-     * acciones posibles de animacion.
-     * @return booleano -false si no esta visible o si esta bloqueada
+     Manda si esta carta esta activada, si se ve la imgen de la carta
+     Si esta bloqueada retornara false puesto que esta carta ya no tiene
+     acciones posibles de animacion.
+     @return booleano -false si no esta visible o si esta bloqueada
      */
     public boolean isSale() {
-        if(bloquear){//bloquear variable que desactiva el giro de la carta de nuevo 
+        if (bloquear) {//bloquear variable que desactiva el giro de la carta de nuevo 
             return false;
         }
         return sale;//=true si no esta
     }
 
+
     /**
-     * Bloquea la imagen esto significa que cuando se ejecute este metodo la carta 
-     * no se podrá mover de nuevo (si la carta se bloquea significa que encontro la
-     * pareja)
+     Bloquea la imagen esto significa que cuando se ejecute este metodo la carta
+     no se podrá mover de nuevo (si la carta se bloquea significa que encontro
+     la pareja)
      */
     public void bloquear() {
-        bloquear=true;
+        bloquear = true;
         this.setIcon(imgCarta);
         repaint();
     }
-    
+
+
     /**
-     * Retorna la direccion de la carta
-     * @return String direccio de la imagen
+     Retorna la direccion de la carta
+     @return String direccio de la imagen
      */
     public String getUrl() {
         return url;
     }
-    
-    
+
+
     /**
      Modifica el tamaño de las imagenes
 
