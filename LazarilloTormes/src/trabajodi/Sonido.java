156f6a93357e3c2906b052e108fb57ccd6a2670c
gmg99 <gmg99@github.com>
2018-11-25 14:01:59 +0100

    er

--- LazarilloTormes/src/trabajodi/Sonido.java
+++ LazarilloTormes/src/trabajodi/Sonido.java
@@ -38,25 +38,24 @@ public class Sonido extends Thread{
      */
     private void repSonido(){
         try {
-           SourceDataLine soundLine = null;//lo creamos a nulo para que si no encuenta la ruta no de error
-           URL url= this.getClass().getResource(sonido); //obtenemos la url del sonido
-           AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);//introduciomos en un AudioInputStream la uta para poder reproducitlo
-           AudioFormat audioFormat = audioInputStream.getFormat();//obtenemos el formato del audio , con el tamaño
-           DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);//obtenemos la informacio, adjuntandole el tamano minimo del buffer interno
-           soundLine = (SourceDataLine) AudioSystem.getLine(info);//ingresaos el sonido para poderlo reproducirlo
-           soundLine.open(audioFormat);//abribos el sondido con el formato del audio correspondiente
-           soundLine.start();//iniciamos el sonido, lo empezamos a repoducir
-           int nBytesRead = 0;
-           byte[] sampledData = new byte[BUFFER_SIZE];
-           while (nBytesRead != -1) {
-              nBytesRead = audioInputStream.read(sampledData, 0, sampledData.length);
-              if (nBytesRead >= 0) {
-                 // Writes audio data to the mixer via this source data line.
-                 soundLine.write(sampledData, 0, nBytesRead);
-              }
-           }
-           soundLine.drain();
-           soundLine.close();
+            SourceDataLine soundLine = null;//lo creamos a nulo para que si no encuenta la ruta no de error
+            URL url= this.getClass().getResource(sonido); //obtenemos la url del sonido
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);//introduciomos en un AudioInputStream la uta para poder reproducitlo
+            AudioFormat audioFormat = audioInputStream.getFormat();//obtenemos el formato del audio , con el tamaño
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);//obtenemos la informacio, adjuntandole el tamano minimo del buffer interno
+            soundLine = (SourceDataLine) AudioSystem.getLine(info);//ingresaos el sonido para poderlo reproducirlo
+            soundLine.open(audioFormat);//abribos el sondido con el formato del audio correspondiente
+            soundLine.start();//iniciamos el sonido, lo empezamos a repoducir
+            int nBytesRead = 0;//iniciaizamos que los bytes de reproduccion son 0
+            byte[] sampledData = new byte[BUFFER_SIZE];//creamos una base con el tamaño del buffer
+            while (nBytesRead != -1) {//mientras que los bytes no sean -1 segimos reproduciendo
+                nBytesRead = audioInputStream.read(sampledData, 0, sampledData.length);//remproducimos con el array del buffer, decimos que no esta parado y el tamaño 
+                if (nBytesRead >= 0) {
+                    soundLine.write(sampledData, 0, nBytesRead);//es cribimos los datos de audio a traves de los datos de origen
+                }
+            }
+            soundLine.drain();//vamos vaciamndo los datos, en caso que el drain tenga datos, bloquea hasta que no tenga ningun dato
+            soundLine.close();//c
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
             System.err.println("Error: "+ex.getMessage());
         }
